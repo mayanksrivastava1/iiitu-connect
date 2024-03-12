d31cb3257759da06b8bfd26ded90761bc305d021
Mayank Srivastava <21317@iiitu.ac.in>
2024-03-12 19:39:02 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
 		// Get rid of the CORS error
 		proxy: {
 			"/api": {
-				target: "https://iiitu-connect.onrender.com",
+				target: "http://localhost:5000",
 				changeOrigin: true,
 				secure: false,
 			},
